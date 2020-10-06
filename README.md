b50c27120a012975119e7759d926691c7c26f2e0
abdulkhaliquem9 <abdulkhaliquem9@gmail.com>
2020-10-06 09:57:37 +0530

    implemented Observable.of module

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 - Added minimal dependencies: frint, react, babel & webpack.
 - Support hot reload.
+- installed `rxjs` & `rxjs-compat` packages. 
+#### Note:
+  rxjs-compat is needed for partial import of modules from rxjs. 
+for Example: 
+> `import { of } from 'rxjs/observable/of';`
 
 ### Yet to be added
 - store, async functions, routing...
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
+// import { Observable } from 'rxjs/Observable'
+import { of } from 'rxjs/observable/of';
 import { observe } from 'frint-react'
 
 function Header(props) {
     console.log('Header props', props)
-return <h1>React Header component in app: {props.name}</h1>
+return <h1>React Header component in app: {props.appName}</h1>
 }
 
 export default observe( app => {
-    return {
-        name: app.getName() // `MyAppNameHere`
-      };
+    console.log('observe app', app)
+    // const props = {appName: app.getOption('name')};
+    return of({appName: app.getOption('name')});
 })(Header)
