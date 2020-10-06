84f55867d1976496236a6ac3407b32f44065ef43
abdulkhaliquem9 <abdulkhaliquem9@gmail.com>
2020-10-06 09:44:42 +0530

    observe HOC demo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { observe } from 'frint-react'
 
-export default function Header(props) {
+function Header(props) {
     console.log('Header props', props)
-    return <h1>React Header component.</h1>
+return <h1>React Header component in app: {props.name}</h1>
 }
+
+export default observe( app => {
+    return {
+        name: app.getName() // `MyAppNameHere`
+      };
+})(Header)
